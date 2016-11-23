1c54974199f0e0bd8d023527b9e32056f4da1c5b
Lucas Falcon <lucas.falcon@etu.univ-lyon1.fr>
2016-11-23 23:31:47 +0100

    ajout structude de donnée pour les parametre de taille

---
+++ Assets/PlayerScaleParameters.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScaleParameters : MonoBehaviour {
+
+    public float sizeScale = 1;
+
+    public float fieldOfView = 70;
+
+    public float speed = 1;
+
+    public float mass = 1;
+
+    public float jumpHeight = 1;
+
+    /**
+        Ajouter d'autres trucs ici
+    */
+
+    public PlayerScaleParameters()
+    {
+        sizeScale = 1;
+        fieldOfView = 70;
+        speed = 1;
+        mass = 1;
+        jumpHeight = 1;
+    }
+    public PlayerScaleParameters(float _sizeScale, float _fov, float _speed, float _mass, float _jmp)
+    {
+        sizeScale = _sizeScale;
+        fieldOfView = _fov;
+        speed = _speed;
+        mass = _mass;
+        jumpHeight = _jmp;
+    }
+}
