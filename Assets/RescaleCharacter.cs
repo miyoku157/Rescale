b0032fa258ea4980d7546b740264610014b93d8e
Lucas Falcon <lucas.falcon@etu.univ-lyon1.fr>
2016-11-29 14:32:38 +0100

    Transition de taille dans le temps plus juste (mais moins styley --> utiliser un truc plus sexy que Lerp a l'avenir) + contrôle sur le temps de transition

--- Assets/RescaleCharacter.cs
+++ Assets/RescaleCharacter.cs
@@ -4,28 +4,36 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class RescaleCharacter : MonoBehaviour {
 
-    public float timeTransition;
+    public float transitionTime;
     public int index;
     public PlayerScaleParameters[] playerScaleParameters;
 
+    bool inSizeTransition;
+    int previousScaleParametersIndex;
+    float transitionBeginTime;
+
 	void Start () {
+        inSizeTransition = false;
         gameObject.GetComponent<FirstPersonController>().m_WalkSpeed=3;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 newScale = new Vector3(playerScaleParameters[index].sizeScale, playerScaleParameters[index].sizeScale, playerScaleParameters[index].sizeScale);
-        float newFov=playerScaleParameters[index].fieldOfView;
-        Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView,newFov,2.0f*Time.deltaTime);
-        gameObject.transform.localScale = Vector3.Lerp(transform.localScale, newScale, 2.0f * Time.deltaTime);
+
+        UpdateSize();
+
         if (Input.GetKeyDown(KeyCode.I))
         {
-            ScaleUp();
-        }else if (Input.GetKeyDown(KeyCode.K))
+            if(!inSizeTransition)
+                ScaleUp();
+
+        } else if (Input.GetKeyDown(KeyCode.K))
         {
-            ScaleDown();
+            if (!inSizeTransition)
+                ScaleDown();
         }
+
         else if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             RaycastHit hit;
@@ -38,7 +46,7 @@ public class RescaleCharacter : MonoBehaviour {
                     controller.ScaleDown();
                 }
             }
-        }else if (Input.GetKeyDown(KeyCode.Mouse1))
+        } else if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -54,12 +62,38 @@ public class RescaleCharacter : MonoBehaviour {
         }
     }
 
-    
+    void UpdateSize()
+    {
+        if (inSizeTransition)
+        {
+            float transitionPercent = (Time.time - transitionBeginTime) / transitionTime;
+
+            Vector3 targetScale = new Vector3(playerScaleParameters[index].sizeScale, playerScaleParameters[index].sizeScale, playerScaleParameters[index].sizeScale);
+            float targetFov = playerScaleParameters[index].fieldOfView;
+
+            if (transitionPercent >= 1.0)
+            {
+                Camera.main.fieldOfView = targetFov;
+                gameObject.transform.localScale = targetScale;
+                inSizeTransition = false;
+                return;
+            }
+
+            Vector3 previousScale = new Vector3(playerScaleParameters[previousScaleParametersIndex].sizeScale, playerScaleParameters[previousScaleParametersIndex].sizeScale, playerScaleParameters[previousScaleParametersIndex].sizeScale);
+            float previousFov = playerScaleParameters[previousScaleParametersIndex].fieldOfView;
+
+            Camera.main.fieldOfView = Mathf.Lerp(previousFov, targetFov, transitionPercent);
+            gameObject.transform.localScale = Vector3.Lerp(previousScale, targetScale, transitionPercent);
+        }
+    }
 
     void ScaleUp()
     {
         if (index == playerScaleParameters.Length - 1)
             return;
+        inSizeTransition = true;
+        previousScaleParametersIndex = index;
+        transitionBeginTime = Time.time;
         index++;
         setScale();
     }
@@ -68,6 +102,9 @@ public class RescaleCharacter : MonoBehaviour {
     {
         if (index == 0)
             return;
+        inSizeTransition = true;
+        previousScaleParametersIndex = index;
+        transitionBeginTime = Time.time;
         index--;
         setScale();
 
@@ -75,13 +112,11 @@ public class RescaleCharacter : MonoBehaviour {
 
     void setScale()
     {
-        
-        
+
         gameObject.GetComponent<Rigidbody>().mass = playerScaleParameters[index].mass;
         FirstPersonController controller = gameObject.GetComponent<FirstPersonController>();
         controller.m_JumpSpeed = playerScaleParameters[index].jumpHeight;
         controller.m_WalkSpeed = playerScaleParameters[index].speed;
-        timeTransition = 2.0f;
         /**
             d'autres trucs a faire ici
         */
