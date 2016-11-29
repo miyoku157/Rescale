804e3b8a285eab334009d2beb7eb55a81c69e658
Lucas Falcon <lucas.falcon@etu.univ-lyon1.fr>
2016-11-29 14:54:29 +0100

    Transition dans le temps du resize des cubes

--- Assets/ObjectController.cs
+++ Assets/ObjectController.cs
@@ -3,43 +3,77 @@ using System.Collections;
 
 public class ObjectController : MonoBehaviour {
 
+    public float transitionTime;
+    public int index = 1;
     public ThingsRescaleParameters[] thingsRescaleParameter;
-    int index=1;
+    
     Vector3 baseScale;
+    bool inSizeTransition;
+    int previousScaleParametersIndex;
+    float transitionBeginTime;
+
     // Use this for initialization
     void Start () {
+        inSizeTransition = false;
         baseScale = transform.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
-	}
+        UpdateSize();
+    }
     public void ScaleUp()
     {
-        if (index == thingsRescaleParameter.Length - 1)
+        if (index == thingsRescaleParameter.Length - 1 || inSizeTransition)
             return;
+        inSizeTransition = true;
+        previousScaleParametersIndex = index;
+        transitionBeginTime = Time.time;
         index++;
-        setScale();
+        //setScale();
     }
 
     public void ScaleDown()
     {
-        if (index == 0)
+        if (index == 0 || inSizeTransition)
             return;
+        inSizeTransition = true;
+        previousScaleParametersIndex = index;
+        transitionBeginTime = Time.time;
         index--;
-        setScale();
+        //setScale();
 
     }
-    void setScale()
+
+    void UpdateSize()
     {
-        if (index == 1)
+        if (inSizeTransition)
         {
-            this.gameObject.transform.localScale = baseScale;
+            float transitionPercent = (Time.time - transitionBeginTime) / transitionTime;
+
+            Vector3 targetScale = new Vector3(thingsRescaleParameter[index].sizeScale, thingsRescaleParameter[index].sizeScale, thingsRescaleParameter[index].sizeScale);
+
+            if (transitionPercent >= 1.0)
+            {
+                gameObject.transform.localScale = targetScale;
+                inSizeTransition = false;
+                return;
+            }
+
+            Vector3 previousScale = new Vector3(thingsRescaleParameter[previousScaleParametersIndex].sizeScale, thingsRescaleParameter[previousScaleParametersIndex].sizeScale, thingsRescaleParameter[previousScaleParametersIndex].sizeScale);
+            
+            gameObject.transform.localScale = Vector3.Lerp(previousScale, targetScale, transitionPercent);
         }
-        else
-        {
-            this.gameObject.transform.localScale = new Vector3(transform.localScale.x * thingsRescaleParameter[index].sizeScale, transform.localScale.y * thingsRescaleParameter[index].sizeScale, transform.localScale.z * thingsRescaleParameter[index].sizeScale);
-        }// this.gameObject.GetComponent<Rigidbody>().mass = playerScaleParameters[index].mass;
     }
+    /*   void setScale()
+       {
+           if (index == 1)
+           {
+               this.gameObject.transform.localScale = baseScale;
+           }
+           else
+           {
+               this.gameObject.transform.localScale = new Vector3(transform.localScale.x * thingsRescaleParameter[index].sizeScale, transform.localScale.y * thingsRescaleParameter[index].sizeScale, transform.localScale.z * thingsRescaleParameter[index].sizeScale);
+           }// this.gameObject.GetComponent<Rigidbody>().mass = playerScaleParameters[index].mass;
+       }*/
 }
